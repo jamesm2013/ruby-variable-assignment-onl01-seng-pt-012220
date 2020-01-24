54d90168cdc1805e3bfe8e185f5fb7909b7abab7
free-hashmap-1506 <2013matthew2013@gmail.com>
2020-01-24 00:04:48 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,5 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 .variable.RUBY_PLATFORM
-local variable = ["greeting" "hello World"]
+greeting =  "hello World"
 
